3f9e5a2b166ad234160ccb6887fa1a4a76f6719e
gguneii <guneysuleymanofa@gmail.com>
2025-04-13 23:04:09 +0400

    ended main

--- guess/src/components/main/Main.jsx
+++ guess/src/components/main/Main.jsx
@@ -71,17 +71,72 @@ function Main() {
       </div>
 
       <div className="flex py-10">
-      <img className="max-w-[33%]"
+        <img
+          className="max-w-[33%]"
           src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_04"
           alt="canta"
         />
-        <img className="max-w-[33%]"
+        <img
+          className="max-w-[33%]"
           src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_03"
           alt="canta"
         />
-        <img className="max-w-[33%]"
+        <img
+          className="max-w-[33%]"
           src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_04"
           alt="canta"
+        />
+      </div>
+      
+      <div className="flex items-center overflow-x-auto">
+        <div className="flex flex-col items-center min-w-[187px] max-w-[480px]">
+          <img
+            src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_962,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_05"
+            alt="smth"
+          />
+          <p className="text-[20px] capitalize pt-5">festival Shop</p>
+        </div>
+
+        <div className="flex flex-col items-center min-w-[187px] max-w-[480px]">
+          <img
+            src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_962,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_06"
+            alt=""
+          />
+          <p className="text-[20px] capitalize pt-5">Shades of the Season </p>
+        </div>
+        <div className="flex flex-col items-center min-w-[187px] max-w-[480px]">
+          <img
+            src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_962,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_07"
+            alt=""
+          />
+
+          <p className="text-[20px] capitalize pt-5">Marciano</p>
+        </div>
+
+        <div className="flex flex-col items-center min-w-[187px] max-w-[480px]">
+          <img
+            src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_962,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV1_10461_08"
+            alt=""
+          />
+          <p className="text-[20px] capitalize pt-5">Outfitting</p>
+        </div>
+      </div>
+
+      <div className="flex py-10">
+        <img
+          className="max-w-[33%]"
+          src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV3_10461_09A"
+          alt=""
+        />
+        <img
+          className="max-w-[33%]"
+          src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV3_10461_09A"
+          alt=""
+        />
+        <img
+          className="max-w-[33%]"
+          src="https://img.guess.com/image/upload/q_auto,f_auto,dpr_auto,w_1280,c_limit/v1/NA/Asset/North%20America/E-Commerce/Guess/Bug%20Number/10461/G_Site_Home_ContentCenter_AprilV3_10461_09A"
+          alt=""
         />
       </div>
     </>
